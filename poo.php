d9634e645c93e2553ee921c48a00f94ba00d9cf0
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-25 16:36:44 +0100

    Surrend again :/

--- poo.php
+++ poo.php
@@ -1,19 +1,27 @@
 <?php
+
 require_once('Warrior.php');
 require_once('Mage.php');
 require_once('Rogue.php');
 require_once('Golem.php');
 require_once('AbyssWatchers.php');
+
 $a = new Warrior();
 $b = new Mage();
 $c = new Rogue();
+
 $monster = new AbyssWatchers(600, 5);
+
+
 $a->setName('newB');
 $a->setLevel(1);
+
 $b->setName('xXxRoXorxXx');
 $b->setLevel(1);
+
 $c->setName('Billy');
 $c->setLevel(1);
+
 while ($monster->isAlive()
     && ($a->isAlive() || $b->isAlive() || $c->isAlive()))
 {
@@ -24,5 +32,6 @@ while ($monster->isAlive()
     $c->attack($monster);
     $monster->attack($c);
 }
+
 // echo '<pre>';
 // var_dump($a, $b, $c);
