13e55dd722654ea4780aedcfff5e0543e9a3aadf
Nicolas Tran <nicolas.tran@supinternet.fr>
2018-01-25 14:32:49 +0100

    added //for other monsters

--- poo.php
+++ poo.php
@@ -10,8 +10,8 @@ require_once('Ghom.php');
 $a = new Warrior();
 $b = new Mage();
 $c = new Rogue();
-$monster = new Lardeur(500, 30);
-$monster = new Golem(1000, 20);
+//$monster = new Lardeur(500, 30);
+//$monster = new Golem(1000, 20);
 $monster = new Ghom(600, 10);
 
 $a->setName('newB');
