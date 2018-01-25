c6dde2e001d7144d946a3eb27715a8f5ea510e6c
Woooooood <wassim.boulatika@supinternet.fr>
2018-01-25 16:45:16 +0100

    add other boss

--- poo.php
+++ poo.php
@@ -1,29 +1,28 @@
 <?php
-
 require_once('Warrior.php');
 require_once('Mage.php');
 require_once('Rogue.php');
+require_once('Lardeur.php');
 require_once('Golem.php');
-require_once('AbyssWatchers.php');
-
+require_once('Ghom.php');
+require_once('Tofu.php');
+require_once('AbyssWatchers.php')
 $a = new Warrior();
 $b = new Mage();
 $c = new Rogue();
-
+//$monster = new Lardeur(500, 30);
+//$monster = new Golem(1000, 20);
+//$monster = new Ghom(600, 10);
 $monster = new AbyssWatchers(600, 5);
-
-
+$monster = new Tofu(600, 10);
 $a->setName('newB');
 $a->setLevel(1);
-
 $b->setName('xXxRoXorxXx');
 $b->setLevel(1);
-
 $c->setName('Billy');
 $c->setLevel(1);
-
 while ($monster->isAlive()
-       && ($a->isAlive() || $b->isAlive() || $c->isAlive()))
+    && ($a->isAlive() || $b->isAlive() || $c->isAlive()))
 {
     $a->attack($monster);
     $monster->attack($a);
@@ -31,7 +30,7 @@ while ($monster->isAlive()
     $monster->attack($b);
     $c->attack($monster);
     $monster->attack($c);
+    $monster->setAp(6);
 }
-
 // echo '<pre>';
 // var_dump($a, $b, $c);
