b063374a0ef52d1bca00fabb2f0318848659d27e
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-25 16:34:54 +0100

    Surrend whith poo.php WASSIM A CASSE MON COMBAT

--- poo.php
+++ poo.php
@@ -3,35 +3,26 @@ require_once('Warrior.php');
 require_once('Mage.php');
 require_once('Rogue.php');
 require_once('Golem.php');
-//require_once('AbyssWatchers.php');
-require_once('PaladinHoly.php');
-require_once('Lardeur.php');
-
+require_once('AbyssWatchers.php');
 $a = new Warrior();
 $b = new Mage();
 $c = new Rogue();
-$d = new PaladinHoly();
-$monster = new Lardeur(500, 30);
-
+$monster = new AbyssWatchers(600, 5);
 $a->setName('newB');
 $a->setLevel(1);
 $b->setName('xXxRoXorxXx');
 $b->setLevel(1);
 $c->setName('Billy');
 $c->setLevel(1);
-$d->setName('Fastfire');
-$d->setLevel(1);
 while ($monster->isAlive()
     && ($a->isAlive() || $b->isAlive() || $c->isAlive()))
 {
-    $d->attack($monster);
     $a->attack($monster);
     $monster->attack($a);
     $b->attack($monster);
     $monster->attack($b);
     $c->attack($monster);
     $monster->attack($c);
-    $d->heal($a);
 }
 // echo '<pre>';
 // var_dump($a, $b, $c);
