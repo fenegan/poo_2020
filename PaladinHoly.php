80ea5c98e9bb595b9092b6dc5340d5de1e29735c
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-25 16:24:20 +0100

    Improved PaladinHoly.php

--- PaladinHoly.php
+++ PaladinHoly.php
@@ -2,7 +2,7 @@
 
 require_once('Character.php');
 
-class PaladinHoly extends Character
+class PaladinHoly extends Warrior
 {
 
     public function __construct()
@@ -11,33 +11,42 @@ class PaladinHoly extends Character
         $this->setIntelligence(20);
         $this->setStrength(20);
     }
-    public function getDamage()
-    {
-        return $this->getStrength() * $this->getLevel();
-    }
-
-    public function takeDamage($damage, FighterInterface $enemy = null)
-    {
-        parent::takeDamage($damage, $enemy);
-    }
 
     public function getHeal()
     {
         return $this->getIntelligence() * $this->getLevel();
     }
 
+    public function takeDamage($damage, FighterInterface $enemy = null, $sacrifice = false)
+    {
+        $armor = $this->getStrength() * 5;
+        $reduction = $armor / ($armor + 300);
+        if (true == $sacrifice)
+        {
+            $reduction = 0;
+        }
+        $effectiveDamage = round($damage - $damage * $reduction);
+        $this->setHp($this->getHp() - $effectiveDamage);
+        $this->log("Takes ".$effectiveDamage.' damage (out of '.$damage.' : '.round($reduction*100).'% blocked), '.$this->getHp().' hp left');
+        if (!$this->isAlive())
+            $this->log('Died');
+    }
+
     public function heal(Character $focus)
     {
-        if ($focus->getHp() !== 0)
+        if ($this->isAlive())
         {
-        $hp = $focus->getHp();
-        $damage = $this->getHp();
-        $damage = $damage * 0.1;
-        $focus->setHp($hp);
-        $this->log("sacrifices " . $damage ." hp for heal " . $focus->getName() . " of " . $hp . " hp");
-        $this->takeDamage($damage);
-        } else {
-            $this->log($focus->getName() . " is already dead");
+            if ($focus->getHp() !== 0)
+            {
+            $hp = $focus->getHp();
+            $damage = $this->getHp();
+            $damage = $damage * 0.1;
+            $focus->setHp($hp);
+            $this->log("sacrifices " . $damage ." hp for heal " . $focus->getName() . " of " . $hp . " hp");
+            $this->takeDamage($damage, null, true);
+            } else {
+                $this->log($focus->getName() . " is already dead");
+            }
         }
     }
 }
