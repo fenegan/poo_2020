a182093bbb127bda9eb6b1199615e685ba085f21
ivandeschaussettes <ivan.milosevic@supinternet.fr>
2018-01-25 16:39:28 +0100

    ADDED named attacks for Rogue

--- Rogue.php
+++ Rogue.php
@@ -8,13 +8,60 @@ class Rogue extends Character
     {
         parent::__construct();
         $this->setAgility(30);
+        $this->setAp(6);
     }
     
     public function getDamage()
     {
         return $this->getAgility() * $this->getLevel();
     }
-    
+    public function trickyTrap(FighterInterface $me ,  FighterInterface $enemy){
+        if ($me->getAp() >= 2) {
+            $me->log('Attacks '.$enemy->getName(). ' with Tricky Trap ');
+            $enemy->takeDamage($me->getDamage() * 2, $me);
+            $me->setAp($me->getAp() - 2);
+            $me->log('Action Points left : ' . $me->getAp());
+        } 
+        else {
+            $me->log("Not enough Action points");
+        }
+        if (!$enemy->isAlive())
+            $me->addXp($enemy->getXpOnDeath());
+        }
+    public function cutThroat(FighterInterface $me ,  FighterInterface $enemy){
+        if ($me->getAp() >= 3) {
+            $me->log('Attacks '.$enemy->getName(). ' with Cut Throat ');
+            $enemy->takeDamage($me->getDamage(), $me);
+            $me->setAp($me->getAp() - 4);
+            $me->log('Action Points left : ' . $me->getAp());
+        } 
+        else {
+            $me->log("Not enough Action points");
+        }
+        if (!$enemy->isAlive())
+            $me->addXp($enemy->getXpOnDeath());
+        }
+    public function attack(FighterInterface $enemy)
+    {
+        if ($this->isAlive())
+        {
+            if ($enemy->isAlive())
+            {
+                if ($enemy->isAlive()) {
+                    $success = rand(0, 1);
+                    if ($success) {
+                        $this->trickyTrap($this,$enemy);
+                    } else {
+                        $this->cutThroat($this,$enemy);
+                    }
+                }
+                else
+                    $this->log("Attacks an already dead enemy (".$enemy->getName().")");
+            }
+            else
+                $this->log('Tries to attack '.$enemy->getName() . ' but is already dead');
+        }
+    }
     public function takeDamage($damage, FighterInterface $enemy = null)
     {
         $dodge_points = $this->getAgility() * 5;
