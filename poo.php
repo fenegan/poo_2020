86e5da80fa828cd61e88d633bf7474b74f5a60fb
Woooooood <wassim.boulatika@supinternet.fr>
2018-01-25 16:57:13 +0100

    add abyss watchers

--- poo.php
+++ poo.php
@@ -2,25 +2,30 @@
 require_once('Warrior.php');
 require_once('Mage.php');
 require_once('Rogue.php');
+require_once('PaladinHoly.php');
 require_once('Lardeur.php');
 require_once('Golem.php');
 require_once('Ghom.php');
 require_once('Tofu.php');
+require_once('Healer.php');
+require_once('BoumBot.php');
 require_once('AbyssWatchers.php');
 $a = new Warrior();
 $b = new Mage();
 $c = new Rogue();
+$d = new PaladinHoly();
+$monster = new AbyssWatchers(600, 5);
 //$monster = new Lardeur(500, 30);
 //$monster = new Golem(1000, 20);
 //$monster = new Ghom(600, 10);
-$monster = new AbyssWatchers(600, 5);
-$monster = new Tofu(600, 10);
 $a->setName('newB');
 $a->setLevel(1);
 $b->setName('xXxRoXorxXx');
 $b->setLevel(1);
 $c->setName('Billy');
 $c->setLevel(1);
+$d->setName('Fastfire');
+$d->setLevel(1);
 while ($monster->isAlive()
     && ($a->isAlive() || $b->isAlive() || $c->isAlive()))
 {
@@ -30,6 +35,7 @@ while ($monster->isAlive()
     $monster->attack($b);
     $c->attack($monster);
     $monster->attack($c);
+    $d->heal($a);
     $monster->setAp(6);
 }
 // echo '<pre>';
