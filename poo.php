41899598b9ad3812474b97b2bf5245ef746ddf44
MrStrikes <yanis.bendahmane@supinternet.fr>
2018-01-25 16:23:22 +0100

    Added time to read the whole fight

--- poo.php
+++ poo.php
@@ -23,16 +23,34 @@ $b->setLevel(1);
 $c->setName('Billy');
 $c->setLevel(1);
 
+$time = 300000;
+
 while ($monster->isAlive()
        && ($a->isAlive() || $b->isAlive() || $c->isAlive()))
 {
     $a->attack($monster);
+    ob_flush();
+    flush();
+    usleep($time);
     $monster->attack($a);
     $b->attack($monster);
+    ob_flush();
+    flush();
+    usleep($time);
     $monster->attack($b);
+    ob_flush();
+    flush();
+    usleep($time);
     $c->attack($monster);
+    ob_flush();
+    flush();
+    usleep($time);
     $monster->attack($c);
+    ob_flush();
+    flush();
+    usleep($time);
 }
+ob_end_flush();
 
 // echo '<pre>';
 // var_dump($a, $b, $c);
